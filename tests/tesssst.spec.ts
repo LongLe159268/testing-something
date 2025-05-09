a1c096ca5e46064493c499e72e788b1f14700286
LongLe <longle159268@gmail.com>
2025-05-09 15:11:01 +0700

    feat: add new thing

--- tests/tesssst.spec.ts
+++ tests/tesssst.spec.ts
@@ -10,6 +10,8 @@ test.describe("Abc", async () => {
             inputDangnhap: '//input[@id= "login_form_username"]',
             inputMatkhau: '//input[@id= "login_form_password"]',
             something: '//header[@class = "ant-layout-header pl-6 justify-between flex items-center pr-6 bg-white border-2 border-b-primary shadow m-4 rounded css-14lptw0"]',
+            inputQuestion: '//div[@contenteditable = "true"]',
+            radioBtn: '//input[@value = "2"]',
 
         };
     });
@@ -23,13 +25,44 @@ test.describe("Abc", async () => {
         await page.getByRole('button', { name: 'Đăng nhập với tư cách Quản trị viên' }).click();
 
         // đăng nhập tk admin
-        await page.locator(xpath.inputDangnhap).fill("admin");
-        await page.locator(xpath.inputMatkhau).fill("1");
+
+        //await page.locator(xpath.inputDangnhap).fill("admin");
+        await page.getByPlaceholder('Tên đăng nhập').pressSequentially('admin', {delay:100});
+        //await page.locator(xpath.inputMatkhau).fill("1");
+        await page.getByPlaceholder('Mật khẩu').pressSequentially('1', {delay:100});
+
         await page.getByRole('button', {name: 'Đăng nhập'}).click();
     });
 
-    test("TC_01: Kiểm tra hiển thị ", async ({page}) => {
-        expect(page.getByText("Phần mềm thi trắc nghiệm").isVisible);
+    test("TC_01:  ", async ({page}) => {
+        // click vào danh sách câu hỏi
+        await page.getByRole("link", {name: "Danh sách câu hỏi"}).click();
+
+        // click vào nút thêm mới
+        await page.getByRole('button', {name: 'Thêm mới'}).click();
+
+        // nhập câu hỏi
+        await page.locator(xpath.inputQuestion).fill("Câu hỏi");
+
+        // nhập đáp án
+        await page.getByPlaceholder('Đáp án 1').fill("A");
+        await page.getByRole('button', {name: 'Thêm đáp án'}).click();
+
+        await page.getByPlaceholder('Đáp án 2').fill("B");
+        await page.getByRole('button', {name: "Thêm đáp án"}).click();
+
+        await page.getByPlaceholder("Đáp án 3").fill("C");
+        await page.getByRole('button', {name: "Thêm đáp án"}).click();
+
+        await page.getByPlaceholder("Đáp án 4").fill("D");
+
+        await page.locator(xpath.radioBtn).check();
+
+        // Lưu câu hỏi
+        await page.getByRole('button', {name: "Lưu câu hỏi"}).click();
     });
 });
+
+
+
 
