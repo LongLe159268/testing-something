4384a17cc2df4b10627c95397429d82d69263935
LongLe <longle159268@gmail.com>
2025-05-10 10:47:55 +0700

    chore: add some small code

--- tests/tesssst.spec.ts
+++ tests/tesssst.spec.ts
@@ -6,9 +6,6 @@ test.describe("Abc", async () => {
     test.beforeAll(async () => {
         basepageURL = "https://ttn.x52.swantech.vn/";
         xpath= {
-            btnDangnhap: '//button[@class = "ant-btn css-14lptw0 ant-btn-primary ant-btn-color-primary ant-btn-variant-solid ant-btn-lg ant-btn-block"]',
-            inputDangnhap: '//input[@id= "login_form_username"]',
-            inputMatkhau: '//input[@id= "login_form_password"]',
             something: '//header[@class = "ant-layout-header pl-6 justify-between flex items-center pr-6 bg-white border-2 border-b-primary shadow m-4 rounded css-14lptw0"]',
             inputQuestion: '//div[@contenteditable = "true"]',
             radioBtn: '//input[@value = "2"]',
@@ -37,36 +34,54 @@ test.describe("Abc", async () => {
     });
 
     test("TC_01:  ", async ({page}) => {
-        // click vào danh sách câu hỏi
-        await page.getByRole("link", {name: "Danh sách câu hỏi"}).click();
-        await page.waitForTimeout(500); // đợi 500ms
+        
+        test.step("Click vào danh sách", async () => {
+            // click vào danh sách câu hỏi
+            await page.getByRole("link", {name: "Danh sách câu hỏi"}).click();
+            await page.waitForTimeout(500); // đợi 500ms
+        });
 
-        // click vào nút thêm mới
-        await page.getByRole('button', {name: 'Thêm mới'}).click();
-        await page.waitForTimeout(500); // đợi 500ms
+        test.step("Click vào nút thêm mới", async () => {
+            // click vào nút thêm mới
+            await page.getByRole('button', {name: 'Thêm mới'}).click();
+            await page.waitForTimeout(500); // đợi 500ms
+        });
 
-        // nhập câu hỏi
-        await page.locator(xpath.inputQuestion).pressSequentially("Câu hỏi", {delay:100});
+        test.step("Nhập câu hỏi", async () => {
+            // nhập câu hỏi
+            await page.locator(xpath.inputQuestion).pressSequentially("Câu hỏi", {delay:100});
+        });
 
-        // nhập đáp án
-        await page.getByPlaceholder('Đáp án 1').pressSequentially("A", {delay:100});
-        await page.getByRole('button', {name: 'Thêm đáp án'}).click();
-        await page.waitForTimeout(500); // đợi 500ms
+        test.step("Nhập đáp án", async () => {
+            // nhập đáp án
+            await page.getByPlaceholder('Đáp án 1').pressSequentially("A", {delay:100});
+            await page.getByRole('button', {name: 'Thêm đáp án'}).click();
+            await page.waitForTimeout(500); // đợi 500ms
 
-        await page.getByPlaceholder('Đáp án 2').pressSequentially("B", {delay:100});
-        await page.getByRole('button', {name: "Thêm đáp án"}).click();
-        await page.waitForTimeout(500); // đợi 500ms
+            await page.getByPlaceholder('Đáp án 2').pressSequentially("B", {delay:100});
+            await page.getByRole('button', {name: "Thêm đáp án"}).click();
+            await page.waitForTimeout(500); // đợi 500ms
 
-        await page.getByPlaceholder("Đáp án 3").pressSequentially("C", {delay:100});
-        await page.getByRole('button', {name: "Thêm đáp án"}).click();
-        await page.waitForTimeout(500); // đợi 500ms
+            await page.getByPlaceholder("Đáp án 3").pressSequentially("C", {delay:100});
+            await page.getByRole('button', {name: "Thêm đáp án"}).click();
+            await page.waitForTimeout(500); // đợi 500ms
 
-        await page.getByPlaceholder("Đáp án 4").pressSequentially("D", {delay:100});
+            await page.getByPlaceholder("Đáp án 4").pressSequentially("D", {delay:100});
+        });
 
-        await page.locator(xpath.radioBtn).check();
+        test.step("Chọn đáp án đúng", async () => {
+            await page.locator(xpath.radioBtn).check();
+        });
 
-        // Lưu câu hỏi
-        await page.getByRole('button', {name: "Lưu câu hỏi"}).click();
+        test.step("Lưu câu hỏi", async () => {
+            // Lưu câu hỏi
+            await page.getByRole('button', {name: "Lưu câu hỏi"}).click();
+        });
+        
+
+        //await expect(page.getByText('Email hoặc mật khẩu không đúng')).toBeVisible();
+
+        //await expect(page.getByRole('button', {name:"Đấp án"})).toBeVisible();
     });
 });
 
